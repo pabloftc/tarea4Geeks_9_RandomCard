c6ac420866efc2df747646ed494d002e74eab655
pabloftc <pabloftc@gmail.com>
2022-01-24 15:08:41 +0000

    Ejercicio Random Card Dealer

--- src/app.js
+++ src/app.js
@@ -2,10 +2,40 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.getElementById("number").innerHTML = randomNumber();
+  let suit = (document.getElementById("top").innerHTML = randomSuit());
+  document.getElementById("bottom").innerHTML = suit;
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function randomNumber() {
+    const numbers = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    let indexNumber = Math.floor(Math.random() * numbers.length);
+    return numbers[indexNumber];
+  }
+  function randomSuit() {
+    const suit = ["♦", "♥", "♠", "♣"];
+    let indexSuit = Math.floor(Math.random() * suit.length);
+    let result = suit[indexSuit];
+    if (result === "♠" || result === "♣") {
+      return result;
+    } else {
+      document.getElementById("top").style.color = "red";
+      document.getElementById("bottom").style.color = "red";
+      return result;
+    }
+  }
 };
